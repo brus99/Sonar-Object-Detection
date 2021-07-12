c53737117de1139889c075a3a72bb7e90671d4b6
brus99 <81587333+brus99@users.noreply.github.com>
2021-07-12 14:17:41 -0700

    Add files via upload

---
+++ MotionTracker.ino
@@ -0,0 +1,48 @@
+#include <Servo.h>       // import arduino's servo library
+Servo s1;                // initialize my servo data type
+long calcDist;
+long cm;
+long dur;
+int currentPosition = 0;
+const int out=12;
+const int in=13;
+void setup() {
+  // put your setup code here, to run once:
+  s1.attach(2);         // attaches servo to pin 2
+  Serial.begin(9600);    // begin serial counter at 9600 baud rate
+  pinMode(in, INPUT);
+  pinMode(out, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(out,LOW);
+  delayMicroseconds(2);
+  digitalWrite(out,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(out,LOW);
+  dur =pulseIn(in,HIGH);
+  calcDist =calcCenti(dur);
+  Serial.println(String(calcDist));
+
+  
+  for (int i = 0; i < 18; i++) {
+    if(calcDist > 10) {
+      currPosition += 10; 
+      servo.write(currPosition);
+    }
+  }
+
+  
+  s1.write(0);
+  delay(500);
+  s1.write(100);
+  delay(500);
+
+  
+  
+
+}
+long calcCenti(long duration){
+  return (dur*.0343)/2; 
+}
